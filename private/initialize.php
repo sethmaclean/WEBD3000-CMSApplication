8e2005d87bae0d36fe70a6e777d070586124e6bd
Seth MacLean <sethgordonmaclean@gmail.com>
2022-02-16 22:35:45 -0400

    Headers and page redirects added

--- private/initialize.php
+++ private/initialize.php
@@ -1,4 +1,6 @@
 <?php
+    ob_start(); // output buffering turned on
+    
     // Assign file paths to PHP constants
     // __FILE__ returns the current path to this file
     // dirname() returns the path to the parent directory
---
+++ public/staff/subjects/new.php
@@ -0,0 +1,17 @@
+<?php 
+
+require_once('../../../private/initialize.php'); 
+
+$test = isset($_GET['test']) ? $_GET['test'] : ''; 
+
+if($test == '404') {
+    error_404();
+} else if($test == '500') {
+    error_500();
+} else if ($test == 'redirect') {
+    redirect_to(url_for('/staff/subjects/index.php'));
+} else {
+    echo "No errors";
+}
+
+?>
